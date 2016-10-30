81b09a5c67521f4d8e79afb29d6b134d09422fe6
Dan Morrison <morrison.danr@gmail.com>
2016-10-30 15:02:29 -0400

    set up onSubmit event that calls StorePicker method goToStore()... bind this to goToStore so it references the component

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import { getFunName } from '../helpers'
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+	goToStore(event) {
+		event.preventDefault();
+		console.log(this.storeInput.value);
+		// first grab text from the box
+		// second transition from / to /store/:storeID
+	}
+
 	render() {
 		return (
-			<form className="store-selector">
+			<form className="store-selector" onSubmit={this.goToStore.bind(this)}>
 				<h2>Please Enter Store</h2>
-				<input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
+				<input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => { this.storeInput = input }} />
 				<button>Submit</button>
 			</form>
 			)
