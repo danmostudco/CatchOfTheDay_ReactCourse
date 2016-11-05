19c40939317aceed368db9631dd6823b94c36b94
Dan Morrison <morrison.danr@gmail.com>
2016-11-05 13:03:31 -0400

    set up firebase by adding base.js to hold reference to Rebase package, export this to App.js and set up componentWillMount() function to set up ref to base's syncState function and pass it the current store name. Then setup componentWillUnMount() function to stop tracking state once the user is no longer on that store

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+	apiKey: "AIzaSyD87Rsj2jtz7GSh7Pmd0dXGXhhC0hesyIw",
+    authDomain: "catch-of-the-day-dan-mo.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-dan-mo.firebaseio.com",
+});
+
+export default base;
