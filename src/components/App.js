6350793997805e5130f3a13a7c73d6219edb0dbb
Dan Morrison <morrison.danr@gmail.com>
2016-11-11 08:15:46 -0500

    remove items from state. set up removeFish function and use fishes[key] = null; since firebase cannot use simple delete method. Then use setState to go ahead and update with fish gone. Do the same with removeFromOrder function but use delete method since this is using localStorage.

--- src/components/App.js
+++ src/components/App.js
@@ -11,9 +11,11 @@ class App extends React.Component{
 		super();
 
 		this.addFish = this.addFish.bind(this);
+		this.updateFish = this.updateFish.bind(this);
+		this.removeFish = this.removeFish.bind(this);
 		this.loadSamples = this.loadSamples.bind(this);
 		this.addToOrder = this.addToOrder.bind(this);
-		this.updateFish = this.updateFish.bind(this);
+		this.removeFromOrder = this.removeFromOrder.bind(this);
 
 		// initial state
 		this.state = {
@@ -65,6 +67,12 @@ class App extends React.Component{
 		this.setState({fishes})
 	}
 
+	removeFish(key) {
+		const fishes = {...this.state.fishes};
+		fishes[key] = null;
+		this.setState({fishes});
+	}
+
 	loadSamples() {
 		this.setState({
 			fishes: sampleFishes
@@ -78,6 +86,12 @@ class App extends React.Component{
 		order[key] = order[key] + 1 || 1
 		// update our state
 		this.setState({ order: order});
+	}
+
+	removeFromOrder(key) {
+		const order = {...this.state.order};
+		delete order[key];
+		this.setState({order});
 	}
 
 	render() {
@@ -97,9 +111,11 @@ class App extends React.Component{
 					fishes={this.state.fishes}
 					order={this.state.order}
 					params={this.props.params}
+					removeFromOrder={this.removeFromOrder}
 				/>
 				<Inventory
-					addFish={this.addFish }
+					addFish={this.addFish}
+					removeFish={this.removeFish}
 					loadSamples={this.loadSamples}
 					fishes={this.state.fishes}
 					updateFish={this.updateFish}
